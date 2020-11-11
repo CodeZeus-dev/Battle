9387747e523337d07c17c9673aca6e768de82cc9
CodeZeus-dev <constantine.akritides@gmail.com>
2020-11-11 19:43:44 +0000

    created a session for a get request

--- app.rb
+++ app.rb
@@ -5,9 +5,18 @@ class Battle < Sinatra::Base
     erb :index
   end
 
+  enable :sessions
+
   post '/names' do
     @name1 = params[:name1]
     @name2 = params[:name2]
+    session[:players_names] = [@name1, @name2]
+    redirect "/names?name1=#{@name1}&name2=#{@name2}"
+  end
+
+  get '/names' do
+    @name1 = session[:players_names][0]
+    @name2 = session[:players_names][1]
     erb :play
   end
 
